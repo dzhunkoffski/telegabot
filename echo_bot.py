87d4d88209fbc1607eec1623a327e3df0166a591
Kirill Dzhunkowski <dzhunkofffski@gmail.com>
2021-12-22 21:00:27 +0300

    Fixed linter errors

--- echo_bot.py
+++ echo_bot.py
@@ -9,12 +9,15 @@ load_dotenv()
 mToken = os.getenv("BOT_TOKEN")
 bot = telebot.TeleBot(mToken)
 
+
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
     bot.reply_to(message, "Just what do you think you’re doing, Dave?")
 
+
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
     bot.reply_to(message, message.text)
+
 
 bot.infinity_polling()
