98b26e30a7ff9b10aab8851e77bd9fd076cb3458
Kirill Dzhunkowski <dzhunkofffski@gmail.com>
2021-12-22 23:54:37 +0300

    never

--- echo_bot.py
+++ echo_bot.py
@@ -10,14 +10,13 @@ bot = telebot.TeleBot(mToken)
 
 
 @bot.message_handler(commands=['start', 'help'])
-
-"""THAT'S OK"""
 def send_welcome(message):
+    """Method"""
     bot.reply_to(message, "Just what do you think you’re doing, Dave?")
 
-"""THAT'S OK"""
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
+    """Method"""
     bot.reply_to(message, message.text)
 
 
