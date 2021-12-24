dc80e63e9105ebcde835e93620f88cd735f0d75d
Kirill Dzhunkowski <dzhunkofffski@gmail.com>
2021-12-25 02:45:16 +0300

    123

--- echo_bot.py
+++ echo_bot.py
@@ -1,22 +1,22 @@
-"""MODULES"""
-import os
-import telebot
-from dotenv import load_dotenv
-
-load_dotenv()
-
-mToken = os.getenv("BOT_TOKEN")
-bot = telebot.TeleBot(mToken)
-
-@bot.message_handler(commands=['start', 'help'])
-def send_welcome(message):
-    """Method"""
-    bot.reply_to(message, "Just what do you think you’re doing, Dave?")
-
-@bot.message_handler(func=lambda m: True)
-def echo_all(message):
-    """Method"""
-    bot.reply_to(message, "Test")
-
-
-bot.infinity_polling()
+# """MODULES"""
+# import os
+# import telebot
+# from dotenv import load_dotenv
+#
+# load_dotenv()
+#
+# mToken = os.getenv("BOT_TOKEN")
+# bot = telebot.TeleBot(mToken)
+#
+# @bot.message_handler(commands=['start', 'help'])
+# def send_welcome(message):
+#     """Method"""
+#     bot.reply_to(message, "Just what do you think you’re doing, Dave?")
+#
+# @bot.message_handler(func=lambda m: True)
+# def echo_all(message):
+#     """Method"""
+#     bot.reply_to(message, "Test")
+#
+#
+# bot.infinity_polling()
