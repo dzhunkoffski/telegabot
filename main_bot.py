005bc5852df43c817c7698584510dc13b1f9f0a4
Kirill Dzhunkowski <dzhunkofffski@gmail.com>
2021-12-25 15:34:50 +0300

    Added 10 new  memes

--- main_bot.py
+++ main_bot.py
@@ -6,13 +6,18 @@ from dotenv import load_dotenv
 
 load_dotenv()
 mToken = os.getenv("BOT_TOKEN")
-MEME_SIZE = 7
+MEME_SIZE = 17
 bot = telebot.TeleBot(mToken)
 
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
     """Method"""
-    bot.reply_to(message, "Чтобы получить мем от бота наипши: 'Хочу мем'")
+    bot.reply_to(message, "Привет, чтобы получить мем от бота наипши: 'Хочу мем'")
+
+@bot.message_handler(commands=['start'])
+def send_help(message):
+    """Method"""
+    bot.reply_to(message, "Просто возьми и напиши 'Хочу мем'")
 
 @bot.message_handler(func=lambda m: True)
 def echo_all(message):
@@ -20,11 +25,11 @@ def echo_all(message):
     if message.text == "Хочу мем":
 
         # Добавить разнообразных фразочек
-        bot.reply_to(message, "Выбираю мем для тебя")
+        bot.reply_to(message, "Выбираю мем для тебя...")
         mem_id = random.randint(1, MEME_SIZE)
         with open('img/meme'+str(mem_id)+'.jpeg', 'rb') as photo:
             bot.send_photo(message.chat.id, photo)
-    elif random.randint(1, 10) == 10:
+    elif random.randint(1, 20) == 10:
         bot.reply_to(message, "Just what do you think you’re doing, Dave?")
     else:
         bot.reply_to(message, "Не понимаю тебя, напиши: 'Хочу мем'")
