708304b8a0117a4e6923896493578daab33e24c7
Kirill Dzhunkowski <dzhunkofffski@gmail.com>
2021-12-25 03:28:45 +0300

    Add send meme feature

--- main_bot.py
+++ main_bot.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 mToken = os.getenv("BOT_TOKEN")
+meme_size = 7
 bot = telebot.TeleBot(mToken)
 
 @bot.message_handler(commands=['start', 'help'])
@@ -17,7 +18,12 @@ def send_welcome(message):
 def echo_all(message):
     """Method"""
     if message.text == "Хочу мем":
-        bot.reply_to(message, "Meme")
+
+        # Добавить разнообразных фразочек
+        bot.reply_to(message, "Выбираю мем для тебя")
+        mem_id = random.randint(1, meme_size)
+        photo = open('img/meme'+str(mem_id)+'.jpeg', 'rb')
+        bot.send_photo(message.chat.id, photo)
     elif random.randint(1, 10) == 10:
         bot.reply_to(message, "Just what do you think you’re doing, Dave?")
     else:
